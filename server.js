3896692d08c41a6e89cba6ad72515ec72837a5b0
김대호 <eogh773@gmail.com>
2022-12-21 17:53:47 +0900

    12/21 error-Handling add

--- server.js
+++ server.js
@@ -6,44 +6,58 @@ const cors = require("cors");
 const app = express();
 const port = 8080;
 
-/* [api] */
 app.use(cors());
-
-app.get("/crawling", async (req, res) => {
-  let reqList = req.query;
-  let dataJson = JSON.parse(reqList.testData);
-  let {
-    crawling_channel_type,
-    crawling_keywords,
-    crawling_period_type,
-    crawling_limit,
-  } = dataJson;
-
-  await crawling(
-    crawling_channel_type,
-    crawling_keywords,
-    crawling_period_type,
-    crawling_limit
-  );
-
-  res.send(crawlingData);
-  crawlingData = []; //api 데이터 전송 후 배열 초기화
-});
 
 app.listen(port, () => {
   console.log(`서버가 ${port}로 실행중입니다.`);
 });
 
+/* [api] */
+app.get("/crawling", async (req, res) => {
+  let reqList = req.query;
+
+  try {
+    if (req.query.crawling_keywords === "") {
+      throw new Error("키워드 미입력 시 에러발생");
+    } else {
+      await crawling(
+        reqList.crawling_channel_type,
+        reqList.crawling_keywords,
+        reqList.crawling_period_type,
+        reqList.crawling_limit
+      );
+      res.send(crawlingData);
+    }
+  } catch (err) {
+    console.log("Error");
+    res.send("crawling_error");
+  }
+
+  crawlingData = []; //api 데이터 전송 후 배열 초기화
+});
+
 /* [crawling] */
 let crawlingData = []; // 크롤링 데이터를 받은 Array
 let crawling = async (potal, crawling_keywords, date, length) => {
+  console.log("포탈", potal);
+
+  console.log(
+    "1.potal :",
+    potal,
+    "- 2.keyword :",
+    crawling_keywords,
+    "- 3.date :",
+    date,
+    "- length :",
+    length
+  );
+
   const browser = await puppeteer.launch({
     headless: false,
     args: ["--window-size=1920,1080"],
   }); // puppeteer 실행 // {headless: false}는 크롤링 과정을 GUI로 확인하기 위해 적용(없어도 이상 x)
 
   const page = await browser.newPage(); // 신규 page 생성
-
   await page.setViewport({
     width: 1920,
     height: 1080,
@@ -52,19 +66,19 @@ let crawling = async (potal, crawling_keywords, date, length) => {
   switch (potal) {
     case "naver":
       await page.goto(
-        `https://search.naver.com/search.naver?where=image&sm=tab_jum&query=${crawling_keywords[0]}`
+        `https://search.naver.com/search.naver?where=image&sm=tab_jum&query=${crawling_keywords}`
       ); //수집채널1-Naver 키워드 검색창으로 이동
       await page.waitForSelector("._listImage"); // * 스크랩하려는 태그가 랜더링 완료되기 전까지 기다린다.
       break;
     case "daum":
       await page.goto(
-        `https://search.daum.net/search?w=img&nil_search=btn&DA=NTB&enc=utf8&q=${crawling_keywords[0]}`
+        `https://search.daum.net/search?w=img&nil_search=btn&DA=NTB&enc=utf8&q=${crawling_keywords}`
       ); //수집채널2-Daum = 키워드 검색창으로 이동
       await page.waitForSelector(".thumb_img");
       break;
     case "google":
       await page.goto(
-        `https://www.google.com/search?q=${crawling_keywords[0]}&source=lnms&tbm=isch`
+        `https://www.google.com/search?q=${crawling_keywords}&source=lnms&tbm=isch`
       ); //수집채널3-Google 키워드 검색창으로 이동
       await page.waitForSelector(".rg_i.Q4LuWd");
       break;
@@ -91,14 +105,14 @@ let crawling = async (potal, crawling_keywords, date, length) => {
             clearInterval(timer);
             resolve();
           }
-        }, 100); // distance로 스크롤 내리는 속도를 조절함(100 빠르지만 데이터를 전부 수집 못하는 에러로 length 100정도까지만/ 200권장 / 300 느림)
+        }, 200); // distance로 스크롤 내리는 속도를 조절함(100 빠르지만 데이터를 전부 수집 못하는 에러로 length 100정도까지만/ 200권장 / 300 느림)
       });
     });
   }
 
   const content = await page.content(); // * 페이지 컨텐츠 생성한다.
 
-  const $ = cheerio.load(content); // cheerio에 컨트츠를 인자로 넣어준다.
+  const $ = cheerio.load(content); // cheerio에 컨텐츠를 인자로 넣어준다.
 
   switch (potal) {
     // 수집채널1-Naver 크롤링 css 선택자 지정
@@ -143,14 +157,13 @@ let crawling = async (potal, crawling_keywords, date, length) => {
       break;
   }
 
-  if (crawlingData.length >= 100) {
-    crawlingData.length = 100;
+  if (crawlingData.length >= length) {
+    crawlingData.length = length;
   } //요청 데이터의 length
 
   /* 부가기능 */
   // await page.click("._listImage"); //부가 기능 - 해당 태그를 클릭
   // await page.screenshot({ path: "screen.png" }); // 부가 기능 - 스크린샷 찍기
   // console.dir(crawlingData.length, { maxArrayLength: null }); // 수집한 전체 데이터
-
   await browser.close(); // 브라우저 종료
 };
